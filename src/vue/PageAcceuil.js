8a389473a338c2025489d304d990fcbfb34ee57f
SteeveRaj <tiavinarilala@email.com>
2024-06-18 14:35:59 +0200

    Modification dans le page Acceuil client et rectication des erreurs

--- src/vue/PageAcceuil.js
+++ src/vue/PageAcceuil.js
@@ -3,23 +3,25 @@ import "../assets/css/page_acceuil.css";
 
 export default function PageAcceuil() {
   return (
-    <div className="container">
-      <div className="product-of-the-day">
-        <h1>Gondor Chic</h1>
-        <p>Le produit du jour est le "pantalon zouk" au prix de 50€</p>
-      </div>
-      <div className="login-box">
-        <form>
-          <div className="input-group">
-            <label htmlFor="username">Pseudo</label>
-            <input type="text" id="username" name="username" />
-          </div>
-          <div className="input-group">
-            <label htmlFor="password">Mot de passe</label>
-            <input type="password" id="password" name="password" />
-          </div>
-          <button type="submit">S'identifier</button>
-        </form>
+    <div className="body0">
+      <div className="container">
+        <div className="product-of-the-day">
+          <h1>Gondor Chic</h1>
+          <p>Le produit du jour est le "pantalon zouk" au prix de 50€</p>
+        </div>
+        <div className="login-box">
+          <form>
+            <div className="input-group">
+              <label htmlFor="username">Pseudo</label>
+              <input type="text" id="username" name="username" />
+            </div>
+            <div className="input-group">
+              <label htmlFor="password">Mot de passe</label>
+              <input type="password" id="password" name="password" />
+            </div>
+            <button type="submit">S'identifier</button>
+          </form>
+        </div>
       </div>
     </div>
   );
--- src/vue/PageAcceuilClient.js
+++ src/vue/PageAcceuilClient.js
@@ -1,57 +1,56 @@
 import * as React from "react";
-import "../assets/css/PageAcceuilClient.css";
+import "../assets/css/styles.css";
+import Caddy from '../assets/images/caddy.jpg';
 
 export default function PageAcceuilClient() {
-  return (<div className="div">
-    <div className="div-2">
-      <div className="div-3">Gondor Chic</div>
-      <div className="div-4">
-        <div className="div-5">Page</div>
-        <div className="div-6">Page</div>
-        <div className="div-7">Page</div>
-        <div className="div-8">Se deconnecter</div>
+  return (
+    <div className="div">
+      <div className="div-2">
+        <div className="div-3">Gondor Chic</div>
+        <div className="div-4">
+          <div className="div-5">Page</div>
+          <div className="div-6">Page</div>
+          <div className="div-7">Page</div>
+          <div className="div-8">Se deconnecter</div>
+        </div>
       </div>
-    </div>
-    <div className="div-9">
-      <div className="div-10">Bonjour, Mr Client</div>
-      <div className="div-11">
-        Le produit du jour est le casque du roi de gondor prix de 100§.
-      </div>
-      <div className="div-12">
-        <div className="div-13">
-          <div className="column">
-            <div className="div-14">
-              <img
-                className="img"
-                src="Casque-du-roi-du-Seigneur-des-Anneaux-Elendil.png"
-              />
-              <div className="div-15">
-                <div className="div-16">
-                  <div className="div-17">
-                    <div className="div-18">Commande</div>
-                    <div className="div-19" />
+      <div className="div-9">
+        <div className="div-10">Bonjour, Mr Client</div>
+        <div className="div-11">
+          Le produit du jour est le casque du roi de gondor prix de 100§.
+        </div>
+        <div className="div-12">
+          <div className="div-13">
+            <div className="column">
+              <div className="div-14">
+                <img className="img" src="Casque-du-roi-du-Seigneur-des-Anneaux-Elendil.png" alt="image du produit du jours"/>
+                <div className="div-15">
+                  <div className="div-16">
+                    <div className="div-17">
+                      <div className="div-18">Commande</div>
+                      <input type="text" className="div-19"/>
+                    </div>
+                    <div className="div-20">
+                      <div className="div-21">Stock</div>
+                      <div className="div-22">100</div>
+                    </div>
+                    <img src={Caddy} alt="Caddy" className="img-2-0"/>
                   </div>
-                  <div className="div-20">
-                    <div className="div-21">Stock</div>
-                    <div className="div-22" />
-                  </div>
-                  <img className="img-2-0" src="caddy.jpg" />
                 </div>
               </div>
             </div>
-          </div>
-          <div className="column-2">
-            <div className="div-23">
-              <div className="div-24">Pseudo</div>
-              <div className="div-25" />
-              <div className="div-26">Mot de passe</div>
-              <div className="div-27" />
-              <div className="div-28">S’IDENTIFIER</div>
+            <div className="column-2">
+              <div className="div-23">
+                <div className="div-24">Pseudo</div>
+                <input type="text" className="div-25" />
+                <div className="div-26">Mot de passe</div>
+                <input type="text" className="div-27" />
+                <div className="div-28">S’IDENTIFIER</div>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
   );
 }
