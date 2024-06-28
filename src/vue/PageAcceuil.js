6d3cb0e3b5d1c98369578d4cd603e8bba0249112
Aro <99348310+AroFifa@users.noreply.github.com>
2024-06-28 19:24:49 +0300

    Authentication

--- src/vue/PageAcceuil.js
+++ src/vue/PageAcceuil.js
@@ -1,15 +1,28 @@
 import * as React from "react";
 import "../assets/css/page_acceuil.css";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { rechercherProduitDuJour } from "../service/product/product-svc";
+import { signIn } from "../service/user/user-svc";
 
 export default function PageAcceuil() {
   const [produitsDuJour, setProduitsDuJour] = useState(null);
+  const login = useRef();
+  const password = useRef();
+
   useEffect(() => {
     rechercherProduitDuJour().then((data) => {
       setProduitsDuJour(data);
     });
   }, []);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const data = await signIn(login.current.value, password.current.value);
+    if (data) {
+      // redirect to "/pageAcceuilClient"
+      window.location.href = "/pageAcceuilClient";
+    }
+  };
 
   return (
     <div className="body0">
@@ -19,14 +32,19 @@ export default function PageAcceuil() {
           <p>Le produit du jour est {produitsDuJour?.libelle}</p>
         </div>
         <div className="login-box">
-          <form>
+          <form onSubmit={handleLogin}>
             <div className="input-group">
               <label htmlFor="username">Pseudo</label>
-              <input type="text" id="username" name="username" />
+              <input type="text" ref={login} id="username" name="username" />
             </div>
             <div className="input-group">
               <label htmlFor="password">Mot de passe</label>
-              <input type="password" id="password" name="password" />
+              <input
+                type="password"
+                ref={password}
+                id="password"
+                name="password"
+              />
             </div>
             <button type="submit">S'identifier</button>
           </form>
