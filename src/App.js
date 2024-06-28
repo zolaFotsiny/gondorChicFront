90bfc62ba6bda3432e51c8ec0740579bd2467f26
zolaFotsiny <98031811+zolaFotsiny@users.noreply.github.com>
2024-06-28 16:54:51 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import "./App.css";
-import { BrowserRouter,Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PageAcceuil from "./vue/PageAcceuil";
 import PageAcceuilClient from "./vue/PageAcceuilClient";
 function App() {
 
 
   return (
-  <BrowserRouter>
-  <Routes>
-    <Route path="/" Component={PageAcceuil}></Route>
-    <Route path="pageAcceuilClient" Component={PageAcceuilClient}></Route>
-  </Routes>
-  </BrowserRouter>
- 
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={PageAcceuil}></Route>
+        <Route path="/pageAcceuilClient" Component={PageAcceuilClient}></Route>
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
