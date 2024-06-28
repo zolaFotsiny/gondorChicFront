381344bba423579c71ab780aaee3606a848f06d8
Aro <99348310+AroFifa@users.noreply.github.com>
2024-06-28 20:18:54 +0300

    Update environment.js

--- src/environment/environment.js
+++ src/environment/environment.js
@@ -2,5 +2,5 @@ const DEV_URL_API = "http://127.0.0.1:8080/";
 const PROD_URL_API = "https://gondorchicback.onrender.com/";
 
 export const environment = {
-  URL_API: DEV_URL_API,
+  URL_API: PROD_URL_API,
 };
